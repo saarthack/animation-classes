a65a118f75156e69b3c03f9bbd51c287890a6e8b
saarthack <ssarthak2003@outlook.com>
2025-04-25 20:26:57 +0530

    swiper2

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
-// Initialize Lenis
-const lenis = new Lenis({
-    autoRaf: true,
-});
-
-// Listen for the scroll event and log the event data
-lenis.on('scroll', (e) => {
-    console.log(e);
-});
-
-var tl = gsap.timeline({
-    scrollTrigger: {
-        trigger: '#page2',
-        start: 'top 0%',
-        end: 'top -100%',
-        scrub: 1,
-        pin: true,
-        // markers:true 
-    }
-})
-
-tl.to('img', {
-    width: '100%',
-})
-tl.to('h1', {
-    opacity: 1
+var swiper = new Swiper('.swiper',{
+    navigation:{
+        nextEl:'.next',
+        prevEl:'.prev'
+    },
+    pagination:{
+        el:'.swiper-pagination',
+        dynamicBullets:true,
+        type:'progressbar'
+    },
+    autoplay:{
+        delay:1000,
+        disableOnInteraction:true,
+    },
+    loop:true
 })
